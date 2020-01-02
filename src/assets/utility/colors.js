10b97da4fb3c43a0e919c833f705e6cf057993bf
raufpratama <pratamarauf8@gmail.com>
2020-01-02 07:47:09 +0700

    react navigation done installed

---
+++ src/assets/utility/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+    primary:'',
+    error:'red',
+    placeholder:'gray'
+}
---
+++ src/component/container/Todo.container.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+
+import { colors } from '../../assets/utility/colors'
+
+export const Todocontainer = () => {
+    const [jabatan,setJabatan] = useState()
+    const [nama,setNama] = useState()
+    const [anggota,setAnggota] = useState([])
+    const [formStatus,setFormStatus] = useState(true)
+
+    const changeJabatan = (e) => {
+        setJabatan(e)
+    }
+
+    const changeNama = (e) => {
+        setNama(e)
+    }
+
+    const submitAnggota = () => {
+        if(jabatan && nama) {
+            const new_data = {
+                nama:nama,
+                jabatan:jabatan,
+            }
+            const submit_data = [...anggota,new_data]
+            setAnggota(submit_data)
+        } else {
+            setFormStatus(false)
+        }
+    }
+
+    const content = (
+        <View style={styles.container}>
+            <Text>Nama</Text>
+            <TextInput style={styles.txt_input} onChangeText={changeNama}></TextInput>
+            <Text>Jabatan</Text>
+            <TextInput style={[styles.txt_input,{borderBottomColor:formStatus ? 'black' : colors.error}]} onChangeText={changeJabatan}></TextInput>
+            <Button onPress={submitAnggota} title='submit'/>
+            {anggota ? anggota.map(anggotas => (
+                <>
+                    <Text>{anggotas.nama}</Text>
+                    <Text>{anggotas.jabatan}</Text>
+                </>
+            )) : null}
+        </View>
+    )
+
+    return content
+}
+
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:'center',
+        alignItems:'center',
+        flex:1
+    },
+    txt_input : {
+        minWidth:100,
+        maxWidth:150,
+        borderBottomWidth:1,
+        borderBottomColor:'black'
+    }
+})
