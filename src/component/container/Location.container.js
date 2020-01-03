43e7588967077d3c80aa70a2e79b303108d3e157
raufpratama <pratamarauf8@gmail.com>
2020-01-03 11:00:43 +0700

    location done

--- src/component/container/Location.container.js
+++ src/component/container/Location.container.js
@@ -1,10 +1,75 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, Platform } from 'react-native'
+import Geolocation from '@react-native-community/geolocation'
+import { Locationpresentational } from '../presentational/Location.presentational'
 
 export const Locationcontainer = () => {
-    return (
-        <View>
-            <Text>Halo dunia</Text>
-        </View>
+    const [location,setLocation] = useState({})
+    const [loading,setLoading] = useState(true)
+
+    async function loadLocation() {
+        try {
+            if(Platform.OS == "ios") {
+                Geolocation.requestAuthorization()
+                Geolocation.getCurrentPosition(async(info,error)=>{
+                    if(error){
+                        setLocation({...location,lat:'0',long:'0'})
+                        setLoading(false)
+                    } else {
+                        let temp = location
+                        const location_data = {
+                            lat:info.coords.latitude.toString(),
+                            lng:info.coords.longitude.toString(),
+                        }
+                        console.log(location_data)
+                        await setLocation(location_data)
+                        setLoading(false)
+                        console.log(location)
+                    }
+                })
+            } else {
+                const granted = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                    {
+                        title:'Location permission',
+                        message:'Todo app rauf mau tau lokasi kamu, kasi tau dong',
+                        buttonPositive:'Izinkan',
+                        buttonNegative:'Tidak'
+                    }
+                )
+                if(granted == PermissionsAndroid.RESULTS.GRANTED) {
+                    Geolocation.getCurrentPosition(async(info,error)=>{
+                        if(error){
+                            setLocation({...location,lat:'0',long:'0'})
+                            setLoading(false)
+                        } else {
+                            let temp = location
+                            const location_data = {
+                                lat:info.coords.latitude,
+                                lng:info.coords.longitude,
+                            }
+                            temp = location_data
+                            await setLocation(location_data)
+                            setLoading(false)
+                        }
+                    })
+                } else {
+                    setLocation({...location,lat:'0',long:'0'})
+                    setLoading(false)
+                }
+            }
+        } catch(e) {
+            console.warn(`terjadi error saat permission ${e}`)
+        }
+    }
+
+    const presentational = (
+        <Locationpresentational
+            loading={loading}
+            location={location}
+            loadlocation={loadLocation}
+        />
     )
+
+    return presentational
 }
--- src/component/presentational/Location.presentational.js
+++ src/component/presentational/Location.presentational.js
@@ -1,10 +1,73 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { View, Text, ActivityIndicator, StyleSheet, Dimensions } from 'react-native'
+import { WebView } from 'react-native-webview'
+import { colors } from '../../assets/utility/colors'
 
-export const Locationpresentational = () => {
-    return (
-        <View>
-            <Text>Halo dunia</Text>
-        </View>
+const screenWidth = Dimensions.get('window').width
+
+export const Locationpresentational = props => {
+    const location_url = `https://www.google.com/maps/@${props.location.lng},${props.location.lat},17z`
+
+    useEffect(() => {
+        props.loadlocation()
+        return () => {
+            props.loadlocation();
+        };
+    }, [])
+
+    let content = (
+        <ActivityIndicator 
+            style={styles.loading} 
+            size={30} 
+            color={colors.error}
+        />
     )
+
+    if(!props.loading) {
+        content = (
+            <View style={styles.container}>
+                <WebView
+                    source={{uri:location_url}}
+                    style={styles.webview_container}
+                />
+                <View style={styles.contet_container}>
+                    <Text style={styles.txt_title}>
+                        Your Location
+                    </Text>
+                    <Text style={styles.txt_subtitle}>
+                        latitude : {props.location.lat}{'\n'}
+                        longitude : {props.location.lng}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+
+    return content;
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    loading:{
+        flex:1,
+        justifyContent:'center',
+        alignSelf:'center'
+    },
+    webview_container:{
+        width:screenWidth,
+        height:180,
+        backgroundColor:colors.placeholder
+    },
+    contet_container:{
+        padding:16
+    },
+    txt_title:{
+        fontSize:20,
+        fontWeight:"bold"
+    },
+    txt_subtitle:{
+
+    }
+})
